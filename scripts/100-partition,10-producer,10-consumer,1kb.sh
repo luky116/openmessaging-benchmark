a913e4c1be0e1d37c1a0e577725f9c8c1383f75f
luky116 <luky116@126.com>
2023-08-08 17:24:36 +0800

    add scripts

---
+++ scripts/100-partition,10-producer,10-consumer,1kb.sh
@@ -0,0 +1,4 @@
+sudo bin/benchmark \
+--output results/$1/'100-partition,10-producer,10-consumer.json' \
+--drivers driver-pulsar/pulsar-batched.yaml \
+workloads/pulsar/100partition-10producer-10consumer-1kb.yaml
---
+++ scripts/run_1city1room.sh
@@ -0,0 +1,4 @@
+sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city1room
+sh scripts/1-partition,10-producer,10-consumer,1kb.sh 1city1room
+sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city1room
+sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city1room
