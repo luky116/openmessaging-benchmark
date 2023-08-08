3c50ae97802ec09843d54fcc8a0ee4b9596e58db
luky116 <luky116@126.com>
2023-08-08 18:30:50 +0800

    异步执行脚本

--- scripts/run_2city2room.sh
+++ scripts/run_2city2room.sh
@@ -1,4 +1,7 @@
-sh scripts/1-partition,1-producer,1-consumer,1kb.sh 2city2room
-sh scripts/1-partition,10-producer,10-consumer,1kb.sh 2city2room
-sh scripts/100-partition,10-producer,10-consumer,1kb.sh 2city2room
-sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 2city2room
+nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 2city2room > p1.log &
+sleep 130s
+nohup sh scripts/1-partition,10-producer,10-consumer,1kb.sh 2city2room > p2.log &
+sleep 130s
+nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 2city2room > p3.log &
+sleep 130s
+nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 2city2room > p4.log &
