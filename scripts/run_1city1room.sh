dc61efd40f43f4062728855e8dbf08741c1f860c
luky116 <luky116@126.com>
2023-08-08 17:56:43 +0800

    add nohup log

--- scripts/run_1city1room.sh
+++ scripts/run_1city1room.sh
@@ -1,7 +1,7 @@
-nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city1room &
+nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city1room > p1.log &
 sleep 130s
-nohup sh scripts/1-partition,10-producer,10-consumer,1kb.sh 1city1room &
+nohup sh scripts/1-partition,10-producer,10-consumer,1kb.sh 1city1room > p2.log &
 sleep 130s
-nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city1room &
+nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city1room > p3.log &
 sleep 130s
-nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city1room &
+nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city1room > p4.log &
