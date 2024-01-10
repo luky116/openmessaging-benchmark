010b533b51061aa6da546154c3545774a1814d5b
liuyuecai <liuyuecai@360.cn>
2024-01-10 17:28:46 +0800

    无需创建NS

--- driver-pulsar/src/main/java/io/openmessaging/benchmark/driver/pulsar/PulsarBenchmarkDriver.java
+++ driver-pulsar/src/main/java/io/openmessaging/benchmark/driver/pulsar/PulsarBenchmarkDriver.java
@@ -127,8 +127,8 @@ public class PulsarBenchmarkDriver implements BenchmarkDriver {
             // 修改，这里直接使用namespacePrefix作为namespace，不再添加后缀，方便控制NS的声明周期
             this.namespace = config.client.namespacePrefix;
 //            this.namespace = config.client.namespacePrefix + "-" + getRandomString();
-            adminClient.namespaces().createNamespace(namespace);
-            log.info("Created Pulsar namespace {}", namespace);
+//            adminClient.namespaces().createNamespace(namespace);
+//            log.info("Created Pulsar namespace {}", namespace);
 
             PersistenceConfiguration p = config.client.persistence;
             adminClient.namespaces().setPersistence(namespace,
