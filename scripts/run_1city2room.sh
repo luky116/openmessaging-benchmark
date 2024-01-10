35da7356c5358088cce2f23de41123e0f0d05335
liuyuecai <liuyuecai@360.cn>
2024-01-10 16:53:09 +0800

    消息大小由1kb设置为100b

--- scripts/run_1city2room.sh
+++ scripts/run_1city2room.sh
@@ -1,7 +1,7 @@
-nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city2room  > p1.log &
+nohup sh scripts/1-partition,1-producer,1-consumer,100b 1city2room  > p1.log &
 sleep 130s
-nohup sh scripts/10-partition,10-producer,10-consumer,1kb.sh 1city2room  > p2.log &
+nohup sh scripts/10-partition,10-producer,10-consumer,100b 1city2room  > p2.log &
 sleep 130s
-nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city2room  > p3.log &
+nohup sh scripts/100-partition,10-producer,10-consumer,100b 1city2room  > p3.log &
 sleep 130s
-nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city2room  > p4.log &
+nohup sh scripts/1000-partition,10-producer,10-consumer,100b 1city2room  > p4.log &
