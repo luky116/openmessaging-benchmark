b90ecebe5cb2c017b0d18c9aa79c84f33565141f
liuyuecai <liuyuecai@360.cn>
2024-01-10 17:26:17 +0800

    回滚：消息大小由1kb设置为100b

--- scripts/run_messageSize.sh
+++ scripts/run_messageSize.sh
@@ -1,4 +1,4 @@
-nohup sh scripts/100-partition,10-producer,10-consumer,100b message_size_test > p1.log &
+nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh message_size_test > p1.log &
 sleep 130s
 nohup sh scripts/100-partition,10-producer,10-consumer,10kb.sh message_size_test > p2.log &
 sleep 130s
