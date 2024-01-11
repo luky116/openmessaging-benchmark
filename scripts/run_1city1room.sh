49b0812efe70d70b5f6f218a0597a7eb0b604b51
liuyuecai <liuyuecai@360.cn>
2024-01-11 12:04:36 +0800

    修改测试时间为5min

--- scripts/run_1city1room.sh
+++ scripts/run_1city1room.sh
@@ -2,10 +2,10 @@ git rm -rf results/1city1room
 mkdir -p results/1city1room
 
 nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city1room > p1.log &
-sleep 130s
+sleep 310s
 nohup sh scripts/10-partition,10-producer,10-consumer,1kb.sh 1city1room > p2.log &
-sleep 130s
+sleep 310s
 nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city1room > p3.log &
-sleep 130s
+sleep 310s
 nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city1room > p4.log &
-sleep 130s
+sleep 310s
