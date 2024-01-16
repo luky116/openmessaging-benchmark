b7e1364c2554a3629b28aa5ceeadb00a1cf9c165
liuyuecai <liuyuecai@360.cn>
2024-01-16 09:54:39 +0800

    rollback

--- scripts/run_1city2room.sh
+++ scripts/run_1city2room.sh
@@ -1,11 +1,7 @@
 nohup sh scripts/1-partition,1-producer,1-consumer,1kb.sh 1city2room  > p1.log &
 sleep 130s
-> p1.log
 nohup sh scripts/10-partition,10-producer,10-consumer,1kb.sh 1city2room  > p2.log &
 sleep 130s
-> p2.log
 nohup sh scripts/100-partition,10-producer,10-consumer,1kb.sh 1city2room  > p3.log &
 sleep 130s
-> p3.log
 nohup sh scripts/1000-partition,10-producer,10-consumer,1kb.sh 1city2room  > p4.log &
-> p4.log
